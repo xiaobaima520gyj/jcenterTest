4d882dc4c5737b7581e21d2b8d0d9f56dddb4562
LiuLiangLiang <1923898264@qq.com>
2018-11-01 11:17:34 +0800

    add a jcenter test java file

---
+++ testdemo/src/main/java/com/kangxin/doctor/testdemo/JcenterDemo.java
@@ -0,0 +1,10 @@
+package com.kangxin.doctor.testdemo;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class JcenterDemo {
+    public void showJcenterTest(Context context, String msg){
+        Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+    }
+}
